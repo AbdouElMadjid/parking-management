cd8b11c47bf6ba215e73d919b074617b526e97ae
aboudou888 <aboudou888@gmail.com>
2025-09-22 11:09:22 +0200

    configuration du database à partir des variables d'encvironnement

--- config/database.js
+++ config/database.js
@@ -1,8 +1,25 @@
+
+require('dotenv').config();
+
 const { Sequelize } = require('sequelize');
 
-const database = new Sequelize('ParkingDB', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+const database = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+{
+        host: process.env.DB_HOST,
+        dialect: 'mysql'
+        //   port: process.env.DB_PORT
+    }
+);
 
 module.exports = database;
+// const { Sequelize } = require('sequelize');
+
+// const database = new Sequelize('ParkingDB', 'root', '', {
+//     host: 'localhost',
+//     dialect: 'mysql'
+// });
+
+// module.exports = database;
